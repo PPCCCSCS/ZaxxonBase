cb7678a82ec063ad4fad76cc65ed8c281830cc67
PPCCCSCS <46571983+PPCCCSCS@users.noreply.github.com>
2022-04-29 11:35:01 -0600

    Add files via upload
    Beginning of rebuild with OOP

---
+++ Triangle.py
@@ -0,0 +1,7 @@
+class Triangle:
+    def __init__(self,
+                 p_0   = [  0,  0,  1],
+                 p_1   = [  0,  1,  0],
+                 p_2   = [  1,  0,  0],
+                 color = [255,  0,255])
+    
